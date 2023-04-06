5a5423ca95415600fb8136651931398afab3e1b6
Vasiliy Gorulev <89159793029@ya.ru>
2023-04-06 19:54:13 +0300

    Добавил домашние задачи после Семинара 3

---
+++ Task_21/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int x1, y1, z1, x2, y2, z2;
+Console.Write("Введите значение X для 1 точки: ");
+x1 = UserInput();
+Console.Write("Введите значение Y для 1 точки: ");
+y1 = UserInput();
+Console.Write("Введите значение Z для 1 точки: ");
+z1 = UserInput();
+Console.Write("Введите значение X для 2 точки: ");
+x2 = UserInput();
+Console.Write("Введите значение Y для 2 точки: ");
+y2 = UserInput();
+Console.Write("Введите значение Z для 2 точки: ");
+z2 = UserInput();
+Console.Write($"Расстояние между точками А ({x1},{y1},{z1}); B ({x2},{y2},{z2}) -> ");
+double res = Distance(x1, y1, z1, x2, y2, z2);
+Console.Write(res.ToString("0.##"));
+
+//_____ Funcs
+
+int UserInput()
+{
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+        Console.Write("Вы ввели не число. Повторите ввод: ");
+
+    return number;
+}
+
+double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+}
---
+++ Task_23/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число, а программа выведет таблицу кубов чисел от 1 до введённого вами: ");
+int a = UserInput();
+Console.Write($"Таблица кубов числа {a}-> ");
+TryTable(a);
+
+
+//_____ Funcs
+
+int UserInput()
+{
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+        Console.Write("Вы ввели не число. Повторите ввод: ");
+
+    return number;
+}
+
+void TryTable(int N)
+{
+    int count = 1;
+    if (N < 0)
+        N *= (-1);
+
+    while (count <= N - 1)
+    {
+        Console.Write(count * count * count + ", ");
+        count++;
+    }
+    Console.Write(count * count * count);
+}
