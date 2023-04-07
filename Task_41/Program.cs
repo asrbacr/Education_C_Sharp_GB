e1aef8f1a4fa69937a3d5ccbd0f37552ede3fc76
Vasiliy Gorulev <89159793029@ya.ru>
2023-04-07 18:47:10 +0300

    Добавил домашние задачи после Семинара 6

---
+++ Task_41/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+//___code
+
+Console.Write("Сколько эл-ов будет в массиве: ");
+int M = UserInput();
+
+int[] Array = FillArray(M);
+
+int Res = CountPositiv(Array);
+
+Console.Write("В массиве "); PrintArray(Array);
+if (Res > 0)
+    Console.Write($" -> {Res} элементов больше 0.");
+else
+    System.Console.Write(" -> нет элементов больше 0.");
+
+
+
+//____funcs
+
+int UserInput()
+{
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+        Console.Write("Вы ввели не число. Повторите ввод: ");
+
+    return number;
+}
+
+int[] FillArray(int a)
+{
+    int[] AddNumbArrya = new int[a];
+    for (int i = 0; i < AddNumbArrya.Length; i++)
+    {
+        System.Console.Write($"Введите {i + 1} элемент: ");
+        AddNumbArrya[i] = UserInput();
+    }
+    return AddNumbArrya;
+}
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    System.Console.Write("[");
+
+    for (int i = 0; i < count - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    System.Console.Write(arr[arr.Length - 1]);
+    // Console.WriteLine();
+    System.Console.Write("]");
+}
+
+int CountPositiv(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count++;
+    }
+    return count;
+}
---
+++ Task_43/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+//___cod
+
+System.Console.WriteLine("Задача найдёт точку пересечения двух прямых.");
+System.Console.Write("Введите значение b1: ");
+int b1 = UserInput();
+System.Console.Write("Введите значение k1: ");
+int k1 = UserInput();
+System.Console.Write("Введите значение b2: ");
+int b2 = UserInput();
+System.Console.Write("Введите значение k2: ");
+int k2 = UserInput();
+
+var x = IntersectionValue(b1: b1, k1: k1, b2: b2, k2: k2);
+
+// На семинаре показали как вывести несколько значений из одной функции.
+Console.WriteLine($"Точка пересечения имеет координаты ({x.Item1.ToString("0.###")}, {x.Item2.ToString("0.###")})");
+
+//___func
+
+int UserInput()
+{
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+        Console.Write("Вы ввели не число. Повторите ввод: ");
+
+    return number;
+}
+
+// Ну да мне было интересно как это сделать, поэтому я спросил.
+(double, double) IntersectionValue(double k1, double k2, double b1, double b2) 
+{
+    return (((b2 - b1) / (k1 - k2)), (k2 * (b2 - b1) / (k1 - k2) + b2));
+}
